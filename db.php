b3c8a59b6ee011fb4211495404a85089ab20a174
ThinvvGCD18359 <thinvvgcd18359@fpt.edu.vn>
2020-06-24 17:46:42 +0700

    new test 2

--- db.php
+++ db.php
@@ -1,7 +1,22 @@
 <?php
-$con = mysqli_connect("ec2-34-206-31-217.compute-1.amazonaws.com","ntdsfcrregsykn","35fabda0ca47320fbf2f48dcab008c70a946d9b5207b243aaaa7fce945928503","d6veukljhn49ai");
-if (mysqli_connect_errno())
-  {
-  echo "Không thể kết nối đến MySQL: " . mysqli_connect_error();
-  }
+
+if (empty(getenv("DATABASE_URL"))){
+    echo '<p>The DB does not exist</p>';
+    $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=mydb', 'postgres', '123456');
+}  else {
+     
+   $db = parse_url(getenv("DATABASE_URL"));
+   $pdo = new PDO("pgsql:" . sprintf(
+        "host=ec2-52-0-155-79.compute-1.amazonaws.com;port=5432;user=uhkywoqfpojzhn;password=e9df249d095cd9b696dc315aa505ef07cfa1d5019e593b3c1a1dd2f9c99b6482;dbname=d9r7n9bel5srhd",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+        ltrim($db["path"], "/")
+   ));
+}  
+
+if($pdo === false){
+     echo "ERROR: Could not connect Database";
+}
 ?>
