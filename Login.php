2367a046c408769f90d59da8d2583999639ffb21
ThinvvGCD18359 <thinvvgcd18359@fpt.edu.vn>
2020-06-24 15:15:20 +0700

    test 8

--- Login.php
+++ Login.php
@@ -4,7 +4,25 @@ session_start();
  
 //Khai báo utf-8 để hiển thị được tiếng việt
 header('Content-Type: text/html; charset=UTF-8');
- 
+ if (empty(getenv("DATABASE_URL"))){
+    echo '<p>The DB does not exist</p>';
+    $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=mydb', 'postgres', '123456');
+}  else {
+     
+   $db = parse_url(getenv("DATABASE_URL"));
+   $pdo = new PDO("pgsql:" . sprintf(
+        "host=ec2-34-206-31-217.compute-1.amazonaws.com;port=5432;user=ntdsfcrregsykn;password=35fabda0ca47320fbf2f48dcab008c70a946d9b5207b243aaaa7fce945928503;dbname=d6veukljhn49ai",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+        ltrim($db["path"], "/")
+   ));
+}  
+
+if($pdo === false){
+     echo "ERROR: Could not connect Database";
+}
 //Xử lý đăng nhập
 if (isset($_POST['Login'])) 
 {
@@ -53,6 +71,8 @@ if (isset($_POST['Login']))
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     </head>
     <body>
+        <h1>REGISTER</h1>
+        <ul>
         <form action='Login.php?do=login' method='POST'>
             <table cellpadding='0' cellspacing='0' border='1'>
                 <tr>
@@ -75,5 +95,6 @@ if (isset($_POST['Login']))
             <input type='submit' value='Login' />
             <a href='Register.php' title='Regiter'>Register</a>
         </form>
+        </ul>
     </body>
 </html>
