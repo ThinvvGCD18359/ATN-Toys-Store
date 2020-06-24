de4ef21ca630894297dde12d40e9b44349432a95
ThinvvGCD18359 <thinvvgcd18359@fpt.edu.vn>
2020-06-24 17:40:46 +0700

    new test 1

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+$con = mysqli_connect("ec2-34-206-31-217.compute-1.amazonaws.com","ntdsfcrregsykn","35fabda0ca47320fbf2f48dcab008c70a946d9b5207b243aaaa7fce945928503","d6veukljhn49ai");
+if (mysqli_connect_errno())
+  {
+  echo "Không thể kết nối đến MySQL: " . mysqli_connect_error();
+  }
+?>
--- login.php
+++ login.php
@@ -1,32 +1,45 @@
-<?php include('functions.php') ?>
+<?php
+?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Registration system PHP and MySQL</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+<meta charset="utf-8">
+<title>Đăng nhập - sharescript.net</title>
+<link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
-	<div class="header">
-		<h2>Login</h2>
-	</div>
-	<form method="post" action="login.php">
-
-		<?php echo display_error(); ?>
-
-		<div class="input-group">
-			<label>Username</label>
-			<input type="text" name="username" >
-		</div>
-		<div class="input-group">
-			<label>Password</label>
-			<input type="password" name="password">
-		</div>
-		<div class="input-group">
-			<button type="submit" class="btn" name="login_btn">Login</button>
-		</div>
-		<p>
-			Not yet a member? <a href="register.php">Sign up</a>
-		</p>
-	</form>
+<?php
+  require('db.php');
+  session_start();
+    if (isset($_POST['username'])){
+    
+    $username = stripslashes($_REQUEST['username']);
+    $username = mysqli_real_escape_string($con,$username);
+    $password = stripslashes($_REQUEST['password']);
+    $password = mysqli_real_escape_string($con,$password);
+        $query = "SELECT * FROM `users` WHERE username='$username' and password='".md5($password)."'";
+    $result = mysqli_query($con,$query) or die(mysql_error());
+    $rows = mysqli_num_rows($result);
+        if($rows==1){
+      $_SESSION['username'] = $username;
+      header("Location: index.php");
+            }else{
+        echo "<div class='form'><h3>Tên đăng nhập hoặc mật khẩu không đúng!</h3></br><a href='login.php'>Đăng nhập lại</a></div>";
+        }
+    }else{
+?>
+<div class="form">
+<h1>Đăng nhập</h1>
+<form action="" method="post" name="login">
+<input type="text" name="username" placeholder="Tên đăng nhập" required />
+<input type="password" name="password" placeholder="Mật khẩu" required />
+<input name="submit" type="submit" value="Đăng nhập" />
+</form>
+<p>Bạn chưa có tài khoản? <a href='registration.php'>Đăng ký ngay</a></p><br/>
+Test ngay:<br/>
+Tên đăng nhập: sharescript.net<br/>
+Mật khẩu: sharescript.net
+</div>
+<?php } ?>
 </body>
 </html>
