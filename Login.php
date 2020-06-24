db0a4a5e1fe41378d767ce02935f7deb034afc58
ThinvvGCD18359 <thinvvgcd18359@fpt.edu.vn>
2020-06-24 16:56:01 +0700

    new test

--- Login.php
+++ Login.php
@@ -1,99 +1,32 @@
+<?php include('functions.php') ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title></title>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    </head>
-    <body>
-        <h1>LOGIN</h1>
-        <ul>
-        <form name="Login" action="Login.php" method="POST" >
-            <table cellpadding='0' cellspacing='0' border='1'>
-                <tr>
-                    <td>
-                        Username :
-                    </td>
-                    <td>
-                        <input type='text' name='username' />
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        Password :
-                    </td>
-                    <td>
-                        <input type='password' name='password' />
-                    </td>
-                </tr>
-            </table>
-            <input type='submit' value='Login' />
-            <a href='Register.php' title='Regiter'>Register</a>
-        </form>
-        </ul>
-        
-        <?php
-        if (empty(getenv("DATABASE_URL"))){
-    echo '<p>The DB does not exist</p>';
-    $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=mydb', 'postgres', '123456');
-}  else {
-     
-   $db = parse_url(getenv("DATABASE_URL"));
-   $pdo = new PDO("pgsql:" . sprintf(
-        "host=ec2-34-206-31-217.compute-1.amazonaws.com;port=5432;user=ntdsfcrregsykn;password=35fabda0ca47320fbf2f48dcab008c70a946d9b5207b243aaaa7fce945928503;dbname=d6veukljhn49ai",
-        $db["host"],
-        $db["port"],
-        $db["user"],
-        $db["pass"],
-        ltrim($db["path"], "/")
-   ));
-}  
+<head>
+	<title>Registration system PHP and MySQL</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="header">
+		<h2>Login</h2>
+	</div>
+	<form method="post" action="login.php">
 
-if($pdo === false){
-     echo "ERROR: Could not connect Database";
-}
-if (isset($_POST["submit"])) {
-		// lấy thông tin người dùng
-		$username = $_POST["username"];
-		$password = $_POST["password"];
-		//làm sạch thông tin, xóa bỏ các tag html, ký tự đặc biệt 
-		//mà người dùng cố tình thêm vào để tấn công theo phương thức sql injection
-		$username = strip_tags($username);
-		$username = addslashes($username);
-		$password = strip_tags($password);
-		$password = addslashes($password);
-		if ($username == "" || $password =="") {
-			echo "username hoặc password bạn không được để trống!";
-		}else{
-			$sql = "select * from customer where username = '$username' and password = '$password' ";
-			$query = mysqli_query($conn,$sql);
-			$num_rows = mysqli_num_rows($query);
-			if ($num_rows==0) {
-				echo "tên đăng nhập hoặc mật khẩu không đúng !";
-			}else
-//$sql = "SELECT username, password FROM customer";
-//if (mysql_num_rows($sql) == 0) {
-  //      echo "Tên đăng nhập này không tồn tại. Vui lòng kiểm tra lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
-    //    exit;
-   // }
-   // $stmt = $pdo->prepare($sql);
-   // if (username != 'username') {
-     //   echo "Tên đăng nhập này không tồn tại. Vui lòng kiểm tra lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
-       // exit;
-    //}
-    //if (password != 'password') {
-     //   echo "Mật khẩu không đúng. Vui lòng nhập lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
-       // exit;
-    //}
-    //else
- {
-    if($stmt->execute() == TRUE){
-        echo "Login successfully.";
-    } else {
-        echo "Please try again ";
-    }
- }
-                }
-}
-?>
-    </body>
+		<?php echo display_error(); ?>
+
+		<div class="input-group">
+			<label>Username</label>
+			<input type="text" name="username" >
+		</div>
+		<div class="input-group">
+			<label>Password</label>
+			<input type="password" name="password">
+		</div>
+		<div class="input-group">
+			<button type="submit" class="btn" name="login_btn">Login</button>
+		</div>
+		<p>
+			Not yet a member? <a href="register.php">Sign up</a>
+		</p>
+	</form>
+</body>
 </html>
--- Register.php
+++ Register.php
@@ -1,74 +1,41 @@
 <!DOCTYPE html>
 <html>
-    <head>
-<title>Insert data to PostgreSQL with php - creating a simple web application</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<style>
-li {
-list-style: none;
-}
-</style>
+<head>
+	<title>Registration system PHP and MySQL</title>
 </head>
 <body>
-<h1>REGISTER</h1>
-<h2>Enter data into table</h2>
-<ul>
-    <form name="Register" action="Register.php" method="POST" >
-<li>Username:</li><li><input type="text" name="username" /></li>
-<li>Email:</li><li><input type="text" name="email" /></li>
-<li>Address:</li><li><input type="text" name="address" /></li>
-<li>Password:</li><li><input type="text" name="password" /></li>
-<li><input type="submit" /></li>
+<div class="header">
+	<h2>Register</h2>
+</div>
+<form method="post" action="register.php">
+	<div class="input-group">
+		<label>Username</label>
+		<input type="text" name="username" value="">
+	</div>
+	<div class="input-group">
+		<label>Email</label>
+		<input type="email" name="email" value="">
+	</div>
+	<div class="input-group">
+		<label>Password</label>
+		<input type="password" name="password_1">
+	</div>
+	<div class="input-group">
+		<label>Confirm password</label>
+		<input type="password" name="password_2">
+	</div>
+	<div class="input-group">
+		<button type="submit" class="btn" name="register_btn">Register</button>
+	</div>
+	<p>
+		Already a member? <a href="login.php">Sign in</a>
+	</p>
 </form>
-</ul>
-
-<?php
-
-if (empty(getenv("DATABASE_URL"))){
-    echo '<p>The DB does not exist</p>';
-    $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=mydb', 'postgres', '123456');
-}  else {
-     
-   $db = parse_url(getenv("DATABASE_URL"));
-   $pdo = new PDO("pgsql:" . sprintf(
-        "host=ec2-34-206-31-217.compute-1.amazonaws.com;port=5432;user=ntdsfcrregsykn;password=35fabda0ca47320fbf2f48dcab008c70a946d9b5207b243aaaa7fce945928503;dbname=d6veukljhn49ai",
-        $db["host"],
-        $db["port"],
-        $db["user"],
-        $db["pass"],
-        ltrim($db["path"], "/")
-   ));
-}  
-
-if($pdo === false){
-     echo "ERROR: Could not connect Database";
-}
-
-//Khởi tạo Prepared Statement
-//$stmt = $pdo->prepare('INSERT INTO student (stuid, fname, email, classname) values (:id, :name, :email, :class)');
-
-//$stmt->bindParam(':id','SV03');
-//$stmt->bindParam(':name','Ho Hong Linh');
-//$stmt->bindParam(':email', 'Linhhh@fpt.edu.vn');
-//$stmt->bindParam(':class', 'GCD018');
-//$stmt->execute();
-//$sql = "INSERT INTO student(stuid, fname, email, classname) VALUES('SV02', 'Hong Thanh','thanhh@fpt.edu.vn','GCD018')";
-$sql = "INSERT INTO customer(username, email, address, password)"
-        . " VALUES('$_POST[username]','$_POST[email]','$_POST[address]','$_POST[password]')";
-$stmt = $pdo->prepare($sql);
-//$stmt->execute();
- if (is_null([username])) {
-   echo "Username must be not null";
- }
- else
- {
-    if($stmt->execute() == TRUE){
-        echo "Register successfully.";
-    } else {
-        echo "Error inserting record: ";
-    }
- }
- 
-?>
+    <?php include('functions.php') ?>
+    <form method="post" action="register.php">
+	<?php echo display_error(); ?>
+    </form>
+    <input type="text" name="username" value="<?php echo $username; ?>">
+    <input type="email" name="email" value="<?php echo $email; ?>">	
 </body>
 </html>
