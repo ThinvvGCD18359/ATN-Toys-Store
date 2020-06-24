61004e23d7eeafe88d90eccc43503f9743a245c2
ThinvvGCD18359 <thinvvgcd18359@fpt.edu.vn>
2020-06-24 18:05:04 +0700

    new test 5

--- login.php
+++ login.php
@@ -8,6 +8,18 @@
 <link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
+    <div class="form">
+<h1>Đăng nhập</h1>
+<form action="login.php" method="post" name="login">
+<input type="text" name="username" placeholder="Tên đăng nhập" required />
+<input type="password" name="password" placeholder="Mật khẩu" required />
+<input name="submit" type="submit" value="Đăng nhập" />
+</form>
+<p>Bạn chưa có tài khoản? <a href='registration.php'>Đăng ký ngay</a></p><br/>
+Test ngay:<br/>
+Tên đăng nhập: sharescript.net<br/>
+Mật khẩu: sharescript.net
+</div>
 <?php
   require('db.php');
   session_start();
@@ -27,19 +39,9 @@
         echo "<div class='form'><h3>Tên đăng nhập hoặc mật khẩu không đúng!</h3></br><a href='login.php'>Đăng nhập lại</a></div>";
         }
     }else{
+        echo "<div class='form'><h3>Đăng nhập thành công!</h3></br><a href='index.php'>Vào trang chủ</a></div>";
+    }
 ?>
-<div class="form">
-<h1>Đăng nhập</h1>
-<form action="login.php" method="post" name="login">
-<input type="text" name="username" placeholder="Tên đăng nhập" required />
-<input type="password" name="password" placeholder="Mật khẩu" required />
-<input name="submit" type="submit" value="Đăng nhập" />
-</form>
-<p>Bạn chưa có tài khoản? <a href='registration.php'>Đăng ký ngay</a></p><br/>
-Test ngay:<br/>
-Tên đăng nhập: sharescript.net<br/>
-Mật khẩu: sharescript.net
-</div>
-<?php } ?>
+
 </body>
 </html>
--- registration.php
+++ registration.php
@@ -8,6 +8,15 @@
 <link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
+    <div class="form">
+<h1>Đăng ký</h1>
+<form name="registration" action="registration.php" method="post">
+<input type="text" name="username" placeholder="Tên đăng nhập" required />
+<input type="email" name="email" placeholder="Email" required />
+<input type="password" name="password" placeholder="Mật khẩu" required />
+<input type="submit" name="submit" value="Đăng ký" />
+</form>
+</div>
 <?php
   require('db.php');
     if (isset($_REQUEST['username'])){
@@ -24,16 +33,8 @@
             echo "<div class='form'><h3>Bạn đã đăng ký thành công</h3><br/>Click để <a href='login.php'>Đăng nhập</a></div>";
         }
     }else{
+        echo "<div class='form'><h3>Đăng ký thất bại</h3><br/>Click để <a href='registration.php'>Đăng kí</a></div>";
+    }
 ?>
-<div class="form">
-<h1>Đăng ký</h1>
-<form name="registration" action="registration.php" method="post">
-<input type="text" name="username" placeholder="Tên đăng nhập" required />
-<input type="email" name="email" placeholder="Email" required />
-<input type="password" name="password" placeholder="Mật khẩu" required />
-<input type="submit" name="submit" value="Đăng ký" />
-</form>
-</div>
-<?php } ?>
 </body>
 </html>
