65e34ffa6e889bcc8d275a0feb85c8a6b0c294f8
ThinvvGCD18359 <thinvvgcd18359@fpt.edu.vn>
2020-06-24 18:15:45 +0700

    test

--- index.php
+++ index.php
@@ -1,18 +1,14 @@
-<?php
-include("auth.php"); ?>
-<!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8">
-<title>Sharescript.net</title>
-<link rel="stylesheet" href="css/style.css" />
-</head>
-<body>
-<div class="form">
-<p>Xin chào <?php echo $_SESSION['username']; ?>!</p>
-<p>Đây là trang chủ</p>
-<p><a href="dashboard.php">Bảng điều khiển</a></p>
-<a href="logout.php">Đăng xuất</a>
-</div>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>ATN Toys Store</title>
+        <h1> Welcome to ATN Store <h1/>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+    <a href="Register.php" target="_blank" >Registration</a>
+     <br/>
 </html>
--- login.php
+++ login.php
@@ -1,47 +1,8 @@
 <?php
-?>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Đăng nhập - sharescript.net</title>
-<link rel="stylesheet" href="css/style.css" />
-</head>
-<body>
-    <div class="form">
-<h1>Đăng nhập</h1>
-<form action="login.php" method="post" name="login">
-<input type="text" name="username" placeholder="Tên đăng nhập" required />
-<input type="password" name="password" placeholder="Mật khẩu" required />
-<input name="submit" type="submit" value="Đăng nhập" />
-</form>
-<p>Bạn chưa có tài khoản? <a href='registration.php'>Đăng ký ngay</a></p><br/>
-Test ngay:<br/>
-Tên đăng nhập: sharescript.net<br/>
-Mật khẩu: sharescript.net
-</div>
-<?php
-  require('db.php');
-  session_start();
-    if (isset($_POST['username'])){
-    
-    $username = stripslashes($_REQUEST['username']);
-    $username = mysqli_real_escape_string($con,$username);
-    $password = stripslashes($_REQUEST['password']);
-    $password = mysqli_real_escape_string($con,$password);
-        $query = "SELECT * FROM `users` WHERE username='$username' and password='".md5($password)."'";
-    $result = mysqli_query($con,$query) or die(mysql_error());
-    $rows = mysqli_num_rows($result);
-        if($rows==1){
-      $_SESSION['username'] = $username;
-      header("Location: index.php");
-            }else{
-        echo "<div class='form'><h3>Tên đăng nhập hoặc mật khẩu không đúng!</h3></br><a href='login.php'>Đăng nhập lại</a></div>";
-        }
-    }else{
-        echo "<div class='form'><h3>Đăng nhập thành công!</h3></br><a href='index.php'>Vào trang chủ</a></div>";
-    }
-?>
 
-</body>
-</html>
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
