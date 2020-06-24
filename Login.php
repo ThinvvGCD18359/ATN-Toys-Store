5b0ede4f1c31423f1ba5f08bb419aaf6625e9cdf
ThinvvGCD18359 <thinvvgcd18359@fpt.edu.vn>
2020-06-24 15:42:33 +0700

    test 10

--- Login.php
+++ Login.php
@@ -1,51 +1,3 @@
-<?php
-//Khai báo sử dụng session
-session_start();
- 
-//Khai báo utf-8 để hiển thị được tiếng việt
-header('Content-Type: text/html; charset=UTF-8');
- 
-//Xử lý đăng nhập
-if (isset($_POST['Login'])) 
-{
-    //Kết nối tới database
-    include('Connect.php');
-     
-    //Lấy dữ liệu nhập vào
-    $username = addslashes($_POST['txtUsername']);
-    $password = addslashes($_POST['txtPassword']);
-     
-    //Kiểm tra đã nhập đủ tên đăng nhập với mật khẩu chưa
-    if (!$username || !$password) {
-        echo "Please input username and password. <a href='javascript: history.go(-1)'>Return</a>";
-        exit;
-    }
-     
-    // mã hóa pasword
-    $password = md5($password);
-     
-    //Kiểm tra tên đăng nhập có tồn tại không
-    $query = mysql_query("SELECT username, password FROM customer WHERE username='$username'");
-    if (mysql_num_rows($query) == 0) {
-        echo "Username is unavailable. Please check again. <a href='javascript: history.go(-1)'>Return</a>";
-        exit;
-    }
-     
-    //Lấy mật khẩu trong database ra
-    $row = mysql_fetch_array($query);
-     
-    //So sánh 2 mật khẩu có trùng khớp hay không
-    if ($password != $row['password']) {
-        echo "Wrong password. Please try again. <a href='javascript: history.go(-1)'>Return</a>";
-        exit;
-    }
-     
-    //Lưu tên đăng nhập
-    $_SESSION['username'] = $username;
-    echo "Hello " . $username . ". Login successfully. <a href='/'>Back to homepage</a>";
-    die();
-}
-?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -78,5 +30,47 @@ if (isset($_POST['Login']))
             <a href='Register.php' title='Regiter'>Register</a>
         </form>
         </ul>
+        
+        <?php
+        if (empty(getenv("DATABASE_URL"))){
+    echo '<p>The DB does not exist</p>';
+    $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=mydb', 'postgres', '123456');
+}  else {
+     
+   $db = parse_url(getenv("DATABASE_URL"));
+   $pdo = new PDO("pgsql:" . sprintf(
+        "host=ec2-34-206-31-217.compute-1.amazonaws.com;port=5432;user=ntdsfcrregsykn;password=35fabda0ca47320fbf2f48dcab008c70a946d9b5207b243aaaa7fce945928503;dbname=d6veukljhn49ai",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+        ltrim($db["path"], "/")
+   ));
+}  
+
+if($pdo === false){
+     echo "ERROR: Could not connect Database";
+}
+    $username = addslashes($_POST['username']);
+    $password = addslashes($_POST['password']);
+    if (!$username || !$password) {
+        echo "Vui lòng nhập đầy đủ tên đăng nhập và mật khẩu. <a href='javascript: history.go(-1)'>Trở lại</a>";
+        exit;
+    }
+    $password = md5($password);
+    $query = mysql_query("SELECT username, password FROM member WHERE username='$username'");
+    if (mysql_num_rows($query) == 0) {
+        echo "Tên đăng nhập này không tồn tại. Vui lòng kiểm tra lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
+        exit;
+    }
+    $row = mysql_fetch_array($query);
+    if ($password != $row['password']) {
+        echo "Mật khẩu không đúng. Vui lòng nhập lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
+        exit;
+    }
+    $_SESSION['username'] = $username;
+    echo "Xin chào " . $username . ". Bạn đã đăng nhập thành công. <a href='/'>Về trang chủ</a>";
+    die();
+?>
     </body>
 </html>
